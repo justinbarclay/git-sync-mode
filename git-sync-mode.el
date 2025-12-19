42bc1ef94f285431f2ea267564f6f5303a3d4081
Justin Barclay <git@justinbarclay.ca>
2025-12-19 10:08:50 -0800

    feat: add guards before running trying to sync
    - no longer run, or error, if nothing to add
    - no longer run, if another git process is in progress

--- git-sync-mode.el
+++ git-sync-mode.el
@@ -55,41 +55,79 @@ git-sync-mode will be enabled."
   "Colourizes the git-sync log buffer for `PROCESS' on `EVENT'."
   (let ((buf (process-buffer process)))
     (when (buffer-live-p buf)
-      (with-current-buffer buf
-        (ansi-color-apply-on-region (point-min) (point-max))
-        (goto-char (point-max))
-        (special-mode)))))
+      (let ((inhibit-read-only t))
+        (with-current-buffer buf
+          (ansi-color-apply-on-region (point-min) (point-max))
+          (goto-char (point-max))
+          (unless (derived-mode-p 'special-mode)
+            (special-mode)))))))
 
-(defun git-sync--execute-command (command &optional dir)
+(defun git-sync--execute-command (command &optional dir ignore-error)
   "Execute `COMMAND' as a promise in the git-sync buffer.
 
 If `DIR' is provided, set `default-directory' to it for the command.
-The promise returns the event passed in by the sentinel functions"
+
+If `IGNORE-ERROR' is non-nil, resolve even if the command fails.
+
+On success the promise returns the process-status for the command
+otherwise it rejects with the process event."
   (let ((dir (or dir default-directory)))
     (promise-new (lambda (resolve reject)
-                   (let ((sentinel-fn (lambda (process event)
+                   (let ((default-directory dir)
+                         (last-output)
+                         (sentinel-fn (lambda (process event)
                                         (git-sync--sentinel-fn process event)
                                         (when (memq (process-status process) '(exit signal))
-                                          (if (zerop (process-exit-status process))
-                                              (funcall resolve event)
+                                          (if (or ignore-error
+                                                  (zerop (process-exit-status process)))
+                                              (funcall resolve last-output)
                                             (funcall reject (format "Command failed: %s" event))))))
-                         (default-directory dir))
+                         (filter-fn (lambda (process string)
+                                      (when (buffer-live-p (process-buffer process))
+                                        (with-current-buffer (process-buffer process)
+                                          (let ((moving (= (point) (process-mark process)))
+                                                (inhibit-read-only 't))
+                                            (save-excursion
+                                              ;; Insert the text, advancing the process marker.
+                                              (goto-char (process-mark process))
+                                              (insert string)
+                                              (set-marker (process-mark process) (point)))
+                                            (if moving (goto-char (process-mark process)))
+                                            (setq last-output string)))))))
                      (make-process :name "git-sync"
-                                   :buffer (get-buffer-create (format "*git-sync:%s*" dir))
+                                   :buffer (get-buffer-create (format "*git-sync:%s*" default-directory))
+                                   :filter filter-fn
                                    :command command
                                    :sentinel sentinel-fn))))))
+
+(async-defun git-sync--has-changes-p (dir)
+  "Return non-nil if there are staged changes in `DIR'."
+  (or (length>
+       (await (git-sync--execute-command '("git" "diff" "--cached" "--name-only") dir))
+       0)
+      (length>
+       (await (git-sync--execute-command '("git" "diff" "--name-only") dir))
+       0)))
+
+(defun git-sync--is-locked-p (dir)
+  "Return non-nil if a .git/index.lock file exists in the repository root of `DIR'."
+  (let* ((root (locate-dominating-file dir ".git"))
+         (lock-file (and root (expand-file-name ".git/index.lock" root))))
+    (and lock-file (file-exists-p lock-file))))
 
 (async-defun git-sync--execute ()
   (let ((dir default-directory)
         (git-message (funcall git-sync-generate-message)))
-    (condition-case err
-        (progn
-          (await (git-sync--execute-command '("git" "add" ".") dir))
-          (await (git-sync--execute-command (list "git" "commit" "-m" git-message) dir))
-          (await (git-sync--execute-command '("git" "pull") dir))
-          (await (git-sync--execute-command '("git" "push") dir))
-          (message "git-sync complete"))
-      (error (message "git-sync failed: %s" err)))))
+    (when (and (await (git-sync--has-changes-p dir))
+               (not (git-sync--is-locked-p dir)))
+      (condition-case err
+          (progn
+            (await (git-sync--execute-command '("git" "add" ".") dir t)) 'exit
+            (await (git-sync--execute-command `("git" "commit" "-m" ,git-message) dir))
+            (await (git-sync--execute-command '("git" "pull") dir))
+            (await (git-sync--execute-command '("git" "push") dir))
+            (message "git-sync complete"))
+        (error (message "git-sync failed: %s" err))))))
 
 (defun git-sync--allowed-directory (current-file)
   "Return non-nil if CURRENT-FILE is in the allow list."
